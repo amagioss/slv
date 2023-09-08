627752d7192d28e3ec9550da3ee22a86ac4d7eae
Shibly Meeran <git@shib.me>
2023-09-09 02:24:33 +0530

    Adding version flag

--- cli/commands/slv.go
+++ cli/commands/slv.go
@@ -1,6 +1,8 @@
 package commands
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -13,12 +15,24 @@ func SlvCommand() *cobra.Command {
 		Short: "SLV is a tool to encrypt secrets locally",
 		Long:  `SLV is a tool for storing and managing secrets in an encrypted format.`,
 		Run: func(cmd *cobra.Command, args []string) {
-			cmd.Help()
+			version, _ := cmd.Flags().GetBool("version")
+			if version {
+				showVersionInfo()
+			} else {
+				cmd.Help()
+			}
 		},
 	}
+	slvCmd.Flags().BoolP("version", "v", false, "Enable preview mode")
 	slvCmd.AddCommand(envCommand())
 	slvCmd.AddCommand(profileCommand())
 	slvCmd.AddCommand(vaultCommand())
 	slvCmd.AddCommand(secretCommand())
 	return slvCmd
+}
+
+var Version = "dev"
+
+func showVersionInfo() {
+	fmt.Println("SLV", Version)
 }
--- go.mod
+++ go.mod
@@ -6,17 +6,17 @@ require (
 	github.com/btcsuite/btcutil v1.0.2
 	github.com/fatih/color v1.15.0
 	github.com/spf13/cobra v1.7.0
-	golang.org/x/crypto v0.12.0
+	golang.org/x/crypto v0.13.0
 )
 
 require (
 	github.com/inconshreveable/mousetrap v1.1.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
-	github.com/mattn/go-isatty v0.0.17 // indirect
+	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 )
 
 require (
-	golang.org/x/sys v0.11.0 // indirect
+	golang.org/x/sys v0.12.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1
 )
