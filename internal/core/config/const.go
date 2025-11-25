fc528ba0b3da1eb12d96e69bc53ede124a5d23b8
Shibly Meeran <shibme@shib.me>
2025-11-26 01:37:02 +0530

    update with a colored ascii art

--- internal/core/config/const.go
+++ internal/core/config/const.go
@@ -1,5 +1,11 @@
 package config
 
+import (
+	"strings"
+
+	"github.com/fatih/color"
+)
+
 const (
 	envar_SLV_APP_DATA_DIR = "SLV_APP_DATA_DIR"
 
@@ -7,23 +13,23 @@ const (
 	AppNameUpperCase = "SLV"
 	description      = AppNameUpperCase + " (Secure Local Vault) : " + "Securely store, share, and access secrets alongside the codebase."
 	website          = "https://slv.sh"
-	Art              = ` ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  
+	art              = ` ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  
 ▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
  ▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
    ▓▓▓▓▓▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒  
-    ▓▓▓▓▓▓▓▓▓▓▓   ▓▓▓▓▓▒▒▒▒▒   
-      ▓▓▓▓▓▓▓▓▓▓ ▓▓▓▓▓▒▒▒▒     
-       ▓▓▓▓▓▓▓▓▓▓▓▓▓▒▒▒▒       
+    ▓▓▓▓▓▓▓▓▓▓▓   ░░░░░▒▒▒▒▒   
+      ▓▓▓▓▓▓▓▓▓▓ ░░░░░▒▒▒▒     
+       ▓▓▓▓▓▓▓▓▓▓░░░▒▒▒▒       
          ▓▓▓▓▓▓▓▓▓▓▒▒▒▒        
            ▓▓▓▓▓▓▓▓▓▓          
-         ▓▓▓▓▓▓▓▓▓▓▓▓▓▓        
-        ▓▓▓▓▓▒▒▒▓▓▓▓▓▓▓▓       
-      ▓▓▓▓▓▓▒▒▒  ▓▓▓▓▓▓▓▓▓     
-    ▓▓▓▓▓▓▒▒▒▒    ▓▓▓▓▓▓▓▓▓▓   
-   ▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  
- ▓▓▓▓▓▓▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
-▓▓▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
- ▓▓▓▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   `
+         ░░░░░▓▓▓▓▓▓▓▓▓        
+        ░░░░░▒▒▒▓▓▓▓▓▓▓▓       
+      ░░░░░░▒▒▒  ▓▓▓▓▓▓▓▓▓     
+    ░░░░░░▒▒▒▒    ▓▓▓▓▓▓▓▓▓▓   
+   ░░░░░░▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓  
+ ░░░░░░▒▒▒▒▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓▓
+░░░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+ ░░░▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒   `
 
 	K8SLVGroup                = "slv.sh"
 	K8SLVVersion              = "v1"
@@ -31,6 +37,12 @@ const (
 	K8SLVAnnotationVersionKey = K8SLVGroup + "/version"
 	K8SLVVaultField           = "spec"
 )
+
+func coloredArt() string {
+	rubyColored := strings.ReplaceAll(art, "▓", color.RGB(157, 58, 79).Sprint("▓"))
+	grayColored := strings.ReplaceAll(rubyColored, "░", color.RGB(79, 85, 89).Sprint("░"))
+	return strings.ReplaceAll(grayColored, "▒", color.RGB(79, 85, 89).Sprint("▒"))
+}
 
 var (
 	Version    = "v" + version
@@ -41,4 +53,6 @@ var (
 	appInfo    *string
 
 	appDataDir *string
+
+	Art = coloredArt()
 )
